d583afddd232ad24532d83b29bcf897bcd29d441
Jerry Xie <jerry_xie@hotmail.com>
2017-11-07 08:38:50 -0500

    Changed project structure.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { IPeople } from './Data Models/ipeople';
+import { ContactService } from './Services/contact.service';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,17 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  title = 'Rosetta';
+  myContactList: IPeople[];
+  matListCheck: string[];
+
+  constructor(private _contactService: ContactService) {
+    this._contactService.getContacts();
+    this.matListCheck = ['fuck', 'shit', 'trash'];
+  }
+
+  OnClick() {
+    this.myContactList = this._contactService.getContacts();
+    this._contactService.tanayTest();
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,30 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import { MatButtonModule,
+         MatListModule,
+         MatMenuModule,
+         MatSidenavModule} from '@angular/material';
 
 import { AppComponent } from './app.component';
+import { ContactService } from './Services/contact.service';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    HttpClientModule,
+    MatButtonModule,
+    MatListModule,
+    MatMenuModule,
+    MatSidenavModule
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+  providers: [ ContactService ],
+  bootstrap: [ AppComponent ]
 })
 export class AppModule { }
